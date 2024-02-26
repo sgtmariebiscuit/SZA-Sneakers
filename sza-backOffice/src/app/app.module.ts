b532c0c06e48e5bfa53869f1b1d9bfd59dcfd22a
Suhail Ally <sgt.mariebiscuit@gmail.com>
2024-02-26 13:54:48 +0200

    Added navigation bar

--- sza-backOffice/src/app/app.module.ts
+++ sza-backOffice/src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './screens/login/login.component';
 import { DashboardComponent } from './screens/dashboard/dashboard.component';
+import { NavigationBarComponent } from './components/navigation-bar/navigation-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavigationBarComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
