aeef3d217c73b14d247e7a3d8c95a8d67a6da2d3
Suhail Ally <sgt.mariebiscuit@gmail.com>
2024-02-27 16:15:53 +0200

    Added class libraries for Repository pattern

---
+++ sza-backend/sza-API/Models/EntityFramework/BaseEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.EntityFramework
+{
+    internal class BaseEntity
+    {
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+        public DateTime DateLastModified { get; set; } = DateTime.Now;
+    }
+}
---
+++ sza-backend/sza-API/Models/Models/Customer.cs
@@ -0,0 +1,11 @@
+﻿namespace sza_API.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string CellNumber { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+    }
+}
