1a6402cb96f103d91c28fedecc893b5176e8e8b0
Suhail Ally <sgt.mariebiscuit@gmail.com>
2024-02-27 15:19:37 +0200

    Implementated model layer

---
+++ sza-backend/sza-API/sza-API/Models/OrderProducts.cs
@@ -0,0 +1,9 @@
+﻿namespace sza_API.Models
+{
+    public class OrderProducts
+    {
+        public int Id { get; set; }
+        public int OrderId { get; set; }
+        public int SneakerId { get; set; }
+    }
+}
