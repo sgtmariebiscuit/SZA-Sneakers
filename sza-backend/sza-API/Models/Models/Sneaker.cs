c98554ec414830ee9a742f09199a4e5d47d1eb50
Suhail Ally <sgt.mariebiscuit@gmail.com>
2024-03-04 16:27:40 +0200

    Name change from sneakers to sneaker in models for naming convention

---
+++ sza-backend/sza-API/Models/Models/Sneaker.cs
@@ -0,0 +1,18 @@
+﻿using Models.EntityFramework;
+
+namespace sza_API.Models
+{
+    public class Sneaker : BaseEntity
+    {
+        public string SupplierId { get; set; }
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Condition { get; set; }
+        public string Size { get; set; }
+        public string SKU { get; set; }
+        public string Availability {  get; set; }
+
+    }
+}
